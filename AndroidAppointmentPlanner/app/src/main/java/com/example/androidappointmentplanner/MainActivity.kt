aaea81df5942af1882770d741bc65f2db5b7e210
bence-d <g.drigan@students.htl-leonding.ac.at>
2023-06-11 16:47:18 +0200

    Elements Created

--- AndroidAppointmentPlanner/app/src/main/java/com/example/androidappointmentplanner/MainActivity.kt
+++ AndroidAppointmentPlanner/app/src/main/java/com/example/androidappointmentplanner/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.androidappointmentplanner
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -9,6 +12,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -19,14 +23,22 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.example.androidappointmentplanner.ui.theme.AndroidAppointmentPlannerTheme
+import java.util.Calendar
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
     // Building database
@@ -114,6 +126,7 @@ fun NewAppointmentForm(
     state: AppointmentState,
     appointmentEvent: (AppointmentEvent) -> Unit
 ) {
+    // Title
     Row() {
         TextField(
             value = state.title,
@@ -124,6 +137,7 @@ fun NewAppointmentForm(
         )
     }
 
+    // Description
     Row() {
         TextField(
             value = state.description,
@@ -134,14 +148,94 @@ fun NewAppointmentForm(
         )
     }
 
+    // Calendar
     Row() {
-        TextField(
-            value = state.date,
-            onValueChange = {
-                appointmentEvent(AppointmentEvent.SetDateEvent(it))
-            },
-            label = { Text(text = "Datum") }
+
+        // Fetching the Local Context
+        val mContext = LocalContext.current
+
+        // Declaring integer values
+        // for year, month and day
+        val mYear: Int
+        val mMonth: Int
+        val mDay: Int
+
+        // Initializing a Calendar
+        val mCalendar = Calendar.getInstance()
+
+        // Fetching current year, month and day
+        mYear = mCalendar.get(Calendar.YEAR)
+        mMonth = mCalendar.get(Calendar.MONTH)
+        mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+
+        mCalendar.time = Date()
+
+        // Declaring a string value to
+        // store date in string format
+        val mDate = remember { mutableStateOf("") }
+
+        // Declaring DatePickerDialog and setting
+        // initial values as current values (present year, month and day)
+        val mDatePickerDialog = DatePickerDialog(
+            mContext,
+            { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+                mDate.value = "$mDayOfMonth/${mMonth+1}/$mYear"
+            }, mYear, mMonth, mDay
         )
+
+        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            // Displaying the mDate value in the Text
+            Text(text = "Selected Date: ${mDate.value}", fontSize = 22.sp, textAlign = TextAlign.Center)
+        }
+
+
+        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(
+                onClick = { mDatePickerDialog.show() }
+            ) {
+                Text(text = "Save")
+            }
+        }
+    }
+
+    // Time
+    Row() {
+        // Fetching local context
+        val mContext = LocalContext.current
+
+        // Declaring and initializing a calendar
+        val mCalendar = Calendar.getInstance()
+        val mHour = mCalendar[Calendar.HOUR_OF_DAY]
+        val mMinute = mCalendar[Calendar.MINUTE]
+
+        // Value for storing time as a string
+        val mTime = remember { mutableStateOf("") }
+
+        // Creating a TimePicker dialod
+        val mTimePickerDialog = TimePickerDialog(
+            mContext,
+            {_, mHour : Int, mMinute: Int ->
+                mTime.value = "$mHour:$mMinute"
+            }, mHour, mMinute, false
+        )
+
+        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            // Displaying the mDate value in the Text
+            Text(text = "Selected Time: ${mTime.value}", fontSize = 22.sp, textAlign = TextAlign.Center)
+        }
+
+
+        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(
+                onClick = { mTimePickerDialog.show() }
+            ) {
+                Text(text = "Save")
+            }
+        }
+    }
+
+    // Save
+    Row() {
         Button(
             onClick = { appointmentEvent(AppointmentEvent.SetAppointmentEvent) }
         ) {
